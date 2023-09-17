14095edcb6c4cd5d0778d7de2dd6c160b1335d12
guilhermev1s <guivs56@gmail.com>
2023-09-17 17:36:12 -0300

    listar filmes

--- screens/filmes/FilmesPopulares.js
+++ screens/filmes/FilmesPopulares.js
@@ -2,6 +2,12 @@ import React from 'react'
 import { Text } from 'react-native-paper'
 
 const FilmesPopulares = () => {
+  function carregar() {
+    apiFilmes.get('/movie/popular?language=pt-BR').then(resultado => {  
+      setFilmes(resultado.data.results)
+    })
+  }
+
   return (
     <>
     <Text>FilmesPopulares</Text>
