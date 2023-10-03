2f8c254d28602ffee4501060e308d2ebb0ce414a
Guilherme Vaz <guivs56@gmail.com>
2023-10-02 21:41:06 -0300

    atores

--- App.js
+++ App.js
@@ -1,7 +1,4 @@
 import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
 import FilmesPopulares from './screens/filmes/FilmesPopulares';
 import { PaperProvider } from 'react-native-paper';
 import FilmesDetalhes from './screens/filmes/FilmesDetalhes';
